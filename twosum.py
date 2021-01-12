f966cd2d5b9737089b30ebc5b40cc3e28e9f4074
jack <darkmatterdontmatter@protonmail.com>
2021-01-11 22:43:32 -0600

    starting twosum

---
+++ twosum.py
@@ -0,0 +1,5 @@
+# EXPLANATION
+# given array of int and a target int,
+# return the indices of two integers in the array that sum to the target
+class Solution:
+    def twosum(self, nums: List[int], target: int) -> List[int]:
