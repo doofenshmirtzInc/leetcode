20e02102501eb24abde89d56ef45032de4668d2d
jack <darkmatterdontmatter@protonmail.com>
2021-01-12 11:48:35 -0600

    starting new problem

---
+++ addnums.py
@@ -0,0 +1,3 @@
+class ListNode:
+    def __init__(self, l1: ListNode, l2: ListNode) -> ListNode:
+        pass
--- twosum.py
+++ twosum.py
@@ -2,6 +2,7 @@
 # given array of int and a target int,
 # return the indices of two integers in the array that sum to the target
 from typing import List
+import random as r
 
 class Solution:
     def twosum(self, nums: List[int], target: int) -> List[int]:
